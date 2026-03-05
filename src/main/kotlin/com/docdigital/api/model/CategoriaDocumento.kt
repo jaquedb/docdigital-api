973438d7e3f9c1c20c85977a5173b123553a8cf2
jaqueline <jaqueline_diasbatista@hotmail.com>
2026-03-05 16:46:05 -0300

    Adição de novas categorias enum

--- src/main/kotlin/com/docdigital/api/model/CategoriaDocumento.kt
+++ src/main/kotlin/com/docdigital/api/model/CategoriaDocumento.kt
@@ -1,11 +1,13 @@
 package com.docdigital.api.model
 
 enum class CategoriaDocumento {
-    NOTA_FISCAL,
-    EXAME_MEDICO,
-    CONTRATO,
-    COMPROVANTE_PAGAMENTO,
+    DOCUMENTO_PESSOAL,
     DOCUMENTO_VEICULAR,
     DOCUMENTO_ACADEMICO,
+    COMPROVANTE_PAGAMENTO,
+    NOTA_FISCAL,
+    CONTRATO,
+    EXAME_MEDICO,
+    RECEITUARIO_MEDICO,
     OUTROS
 }
